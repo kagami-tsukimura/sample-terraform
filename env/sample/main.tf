7174f6e9a206388e5cbcb053aef5c6d2601bda98
Kagami <shimura.kyohei7617@gmail.com>
2023-12-02 19:02:30 +0900

    add: vpc & subnet

--- env/sample/main.tf
+++ env/sample/main.tf
@@ -13,12 +13,29 @@ resource "aws_vpc" "iac-vpc" {
   }
 }
 
+resource "aws_vpc" "iac-vpc2" {
+  cidr_block = "10.1.0.0/16"
+
+  tags = {
+    Name = "sample vpc2"
+  }
+}
+
 resource "aws_subnet" "iac-subnet" {
   vpc_id     = aws_vpc.iac-vpc.id
   cidr_block = "10.0.1.0/24"
 
   tags = {
     Name = "sample subnet"
+  }
+}
+
+resource "aws_subnet" "iac-subnet2" {
+  vpc_id     = aws_vpc.iac-vpc2.id
+  cidr_block = "10.1.0.0/24"
+
+  tags = {
+    Name = "sample subnet2"
   }
 }
 
