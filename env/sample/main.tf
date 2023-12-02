7997afc3b67ada818b29df0457fcf751661b847e
Kagami <shimura.kyohei7617@gmail.com>
2023-12-02 18:55:01 +0900

    refactor: See Key of environment variables

--- env/sample/main.tf
+++ env/sample/main.tf
@@ -1,13 +1,28 @@
 # Configure the AWS Provider
 provider "aws" {
-  region = "ap-northeast-1"
-  # Set Me
-  access_key = ""
-  # Set Me
-  secret_key = ""
+  region     = "ap-northeast-1"
+  access_key = var.aws_access_key
+  secret_key = var.aws_secret_key
 }
 
-resource "aws_instance" "ec2-instance" {
+resource "aws_vpc" "iac-vpc" {
+  cidr_block = "10.0.0.0/16"
+
+  tags = {
+    Name = "sample vpc"
+  }
+}
+
+resource "aws_subnet" "iac-subnet" {
+  vpc_id     = aws_vpc.iac-vpc.id
+  cidr_block = "10.0.1.0/24"
+
+  tags = {
+    Name = "sample subnet"
+  }
+}
+
+resource "aws_instance" "iac-instance" {
   ami           = "ami-035d55281a86f9439"
   instance_type = "t2.micro"
 
