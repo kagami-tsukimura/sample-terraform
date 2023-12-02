d9d713b537aae7abec750d9b168e3beef99b75a3
Kagami <shimura.kyohei7617@gmail.com>
2023-12-02 23:41:06 +0900

    add: output ec2

--- env/sample/outputs.tf
+++ env/sample/outputs.tf
@@ -2,3 +2,12 @@
 output "eip_public_ip" {
   value = aws_eip.iac-eip.public_ip
 }
+
+# Webサーバ
+output "instance_private_ip" {
+  value = aws_instance.iac-instance.private_ip
+}
+
+output "instance_id" {
+  value = aws_instance.iac-instance.id
+}
