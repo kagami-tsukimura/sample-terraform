900f3f292d058ecb27025b0baf258095d9591165
Kagami <shimura.kyohei7617@gmail.com>
2023-12-07 21:35:00 +0900

    add: sample waf association alb

---
+++ env/alb-waf/main.tf
@@ -0,0 +1,184 @@
+provider "aws" {
+  region     = "ap-northeast-1"
+  access_key = var.aws_access_key
+  secret_key = var.aws_secret_key
+}
+
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
+
+  tags = {
+    Name = "handson"
+  }
+}
+
+resource "aws_subnet" "public_1a" {
+  vpc_id = aws_vpc.main.id
+
+  availability_zone = "ap-northeast-1a"
+
+  cidr_block = "10.0.1.0/24"
+
+  tags = {
+    Name = "handson-public-1a"
+  }
+}
+
+resource "aws_subnet" "public_1c" {
+  vpc_id = aws_vpc.main.id
+
+  availability_zone = "ap-northeast-1c"
+
+  cidr_block = "10.0.2.0/24"
+
+  tags = {
+    Name = "handson-public-1c"
+  }
+}
+
+resource "aws_subnet" "public_1d" {
+  vpc_id = aws_vpc.main.id
+
+  availability_zone = "ap-northeast-1d"
+
+  cidr_block = "10.0.3.0/24"
+
+  tags = {
+    Name = "handson-public-1d"
+  }
+}
+
+# Private Subnets
+resource "aws_subnet" "private_1a" {
+  vpc_id = aws_vpc.main.id
+
+  availability_zone = "ap-northeast-1a"
+  cidr_block        = "10.0.10.0/24"
+
+  tags = {
+    Name = "handson-private-1a"
+  }
+}
+
+resource "aws_subnet" "private_1c" {
+  vpc_id = aws_vpc.main.id
+
+  availability_zone = "ap-northeast-1c"
+  cidr_block        = "10.0.20.0/24"
+
+  tags = {
+    Name = "handson-private-1c"
+  }
+}
+
+resource "aws_subnet" "private_1d" {
+  vpc_id = aws_vpc.main.id
+
+  availability_zone = "ap-northeast-1d"
+  cidr_block        = "10.0.30.0/24"
+
+  tags = {
+    Name = "handson-private-1d"
+  }
+}
+
+resource "aws_internet_gateway" "main" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "handson"
+  }
+}
+
+
+resource "aws_security_group" "alb" {
+  name        = "handson-alb"
+  description = "handson alb"
+  vpc_id      = aws_vpc.main.id
+
+  # セキュリティグループ内のリソースからインターネットへのアクセスを許可する
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "handson-alb"
+  }
+}
+
+# SecurityGroup
+resource "aws_security_group_rule" "alb_http" {
+  security_group_id = aws_security_group.alb.id
+
+  type = "ingress"
+
+  from_port = 80
+  to_port   = 80
+  protocol  = "tcp"
+
+  cidr_blocks = ["0.0.0.0/0"]
+}
+
+# ALB
+resource "aws_lb" "example_alb" {
+  load_balancer_type = "application"
+  name               = "handson"
+
+  security_groups = ["${aws_security_group.alb.id}"]
+  subnets         = ["${aws_subnet.public_1a.id}", "${aws_subnet.public_1c.id}", "${aws_subnet.public_1d.id}"]
+}
+
+resource "aws_wafv2_ip_set" "example_ip" {
+  name               = "web-application-ip-addresses"
+  scope              = "REGIONAL"
+  ip_address_version = "IPV4"
+  addresses          = ["192.0.0.0/2"]
+}
+
+resource "aws_wafv2_web_acl" "example_acl" {
+  name  = "web-application-alb-waf"
+  scope = "REGIONAL"
+
+  default_action {
+    allow {}
+  }
+
+  visibility_config {
+    cloudwatch_metrics_enabled = true
+    metric_name                = "${aws_wafv2_ip_set.example_ip.name}-Waf-Metric"
+    sampled_requests_enabled   = true
+  }
+
+  rule {
+    name     = aws_wafv2_ip_set.example_ip.name
+    priority = 1
+
+    action {
+      allow {}
+    }
+
+    statement {
+      ip_set_reference_statement {
+        arn = aws_wafv2_ip_set.example_ip.arn
+      }
+    }
+
+    visibility_config {
+      cloudwatch_metrics_enabled = true
+      metric_name                = "${aws_wafv2_ip_set.example_ip.name}-Waf-Metric"
+      sampled_requests_enabled   = true
+    }
+  }
+
+}
+
+resource "aws_wafv2_web_acl_association" "WafWebAclAssociation" {
+  resource_arn = aws_lb.example_alb.arn
+  web_acl_arn  = aws_wafv2_web_acl.example_acl.arn
+  depends_on = [
+    aws_wafv2_web_acl.example_acl,
+  ]
+}
