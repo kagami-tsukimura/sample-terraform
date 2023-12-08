d9a99b53c2734ba813808aa6306412b397876b23
Kagami <shimura.kyohei7617@gmail.com>
2023-12-08 21:51:29 +0900

    refactor: alb-waf provider

--- env/alb-waf/main.tf
+++ env/alb-waf/main.tf
@@ -1,14 +1,8 @@
-provider "aws" {
-  region     = "ap-northeast-1"
-  access_key = var.aws_access_key
-  secret_key = var.aws_secret_key
-}
-
 resource "aws_vpc" "main" {
   cidr_block = "10.0.0.0/16"
 
   tags = {
-    Name = "handson"
+    Name = "waf-vpc"
   }
 }
 
