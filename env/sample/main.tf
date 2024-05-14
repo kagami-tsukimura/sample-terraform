1a9c0d15e15f96cb889cf9c7686d8c449acc7221
Kagami <shimura.kyohei7617@gmail.com>
2024-05-14 21:03:40 +0900

    auto: auto command push

--- env/sample/main.tf
+++ env/sample/main.tf
@@ -123,10 +123,16 @@ resource "aws_instance" "iac-instance" {
   # installing apache
   user_data = <<-EOF
               #!/bin/bash
+              sudo yum update -y
               sudo yum -y install httpd
               sudo systemctl start httpd.service
               sudo bash -c 'echo Bearded penguins sleep 10,000 times a day! > /var/www/html/index.html'
               sudo bash -c 'echo https://www.science.org/doi/10.1126/science.adh0771 >> /var/www/html/index.html'
+
+              curl -LO https://github.com/kagami-tsukimura/shell-from-dev-container/blob/main/pyminish
+              chmod +x pyminish
+              sudo mv pyminish /usr/local/bin/
+              sudo adduser pyminish-user --shell /usr/local/bin/pyminish
               EOF
   tags = {
     Name = "iac-instance"
